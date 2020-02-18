6d876f01cedbfb4bcc748d931d85bc63c3277ec5
ajikawa <stormsnow1221@gmail.com>
2020-02-18 15:14:40 +0000

    autodeploytest

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'comments/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to 'boards#index'
+  root 'boards#index'
   resources :boards
   resources :comments, only: %i[create destroy]
 end
