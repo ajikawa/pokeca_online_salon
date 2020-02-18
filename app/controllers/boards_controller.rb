216d621e3a7e1d38ce465b152391983b31950d89
ajikawa <stormsnow1221@gmail.com>
2020-02-18 18:48:00 +0000

    #11

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,19 +1,22 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
-    @boards = Board.page(params[:page])
+    @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+    @boards = @boards.page(params[:page])
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
   end
 
   def create
     board = Board.new(board_params)
     if board.save
-    flash[:notice] = "「#{board.title}」のスレッドが作成されました"
-    redirect_to board
+      flash[:notice] = "「#{board.title}」のスレッドを作成しました"
+      redirect_to board
     else
-      redirect_to new_board_path, flash: {
+      redirect_to :back, flash: {
         board: board,
         error_messages: board.errors.full_messages
       }
@@ -21,31 +24,36 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
     @comment = Comment.new(board_id: @board.id)
   end
 
   def edit
-    @board = Board.find(params[:id])
+    @board.attributes = flash[:board] if flash[:board]
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
-
-    redirect_to board
+    if @board.update(board_params)
+      redirect_to @board
+    else
+      redirect_to :back, flash: {
+        board: @board,
+        error_messages: @board.errors.full_messages
+      }
+    end
   end
 
   def destroy
-    board = Board.find(params[:id])
-    board.delete
-
-    redirect_to boards_path, flash: {notice: "「#{board.title}」のスレッドが削除されました"}
+    @board.delete
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」のスレッドが削除されました" }
   end
 
   private
 
   def board_params
-    params.require(:board).permit(:name, :title, :body)
+    params.require(:board).permit(:name, :title, :body, tag_ids: [])
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #開発環境でのページネーションテスト用データ
 if Rails.env == 'development'
-    (1..50).each do |i|
-        Board.create(name: "ユーザー#{i}", title:"タイトル#{i}", body: "本文#{i}")
-    end
+#  (1..50).each do |i|
+#    Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
+#  end
+
+  Tag.create([
+    { name: 'デッキ構築' },
+    { name: '環境考察' },
+    { name: '大会情報' },
+    { name: 'グッズ情報' },
+  ])
+  
+  Tag.delete([
+   { name: 'Ruby' },
+    { name: 'Ruby on Rails4' },
+    { name: 'Ruby on Rails5' },
+    { name: 'Python2' },
+    { name: 'Python3' },
+    { name: 'Django2' }
+  ])
 end
