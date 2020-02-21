69b1cf499b8ac1569d951d7ed1544b2a26a46b43
ajikawa <stormsnow1221@gmail.com>
2020-02-21 08:40:20 +0000

    honban

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'board#index'
+  root 'boards#index'
   resources :boards
   resources :comments, only: %i[create destroy]
 end
