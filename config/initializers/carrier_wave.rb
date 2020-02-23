5b18d6c8b6468d4ea493d3f9ec81ca58e54ceef2
ajikawa <stormsnow1221@gmail.com>
2020-02-23 11:19:05 +0000

    #13 S3設定

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,11 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :aws_access_key_id => ENV['ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['SECRET_ACCESS_KEY'],
-      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      
+      provider: 'AWS',
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
     }
-    config.fog_directory     =  ENV['pokecaonlinesalon']
+    config.fog_directory  = 'pokecaonlinesalon'
   end
 end
